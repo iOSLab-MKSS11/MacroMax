28e38a88807ea14575adbccdb05c362542239d23
Sonzzini <paulo.sonzzini@gmail.com>
2025-10-04 11:16:52 -0300

    Initial Commit

---
+++ MacroMax/MacroMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroMaxApp.swift
+//  MacroMax
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 04/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MacroMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacroMaxTests/MacroMaxTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacroMaxTests.swift
+//  MacroMaxTests
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 04/10/25.
+//
+
+import Testing
+@testable import MacroMax
+
+struct MacroMaxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
